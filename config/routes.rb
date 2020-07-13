c73528501c8713104e1f144c714f368f840645e6
vagrant <vagrant@localhost.localdomain>
2020-07-13 05:04:59 +0000

    [update]home_about_ADD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :home, only: [:index] do
+    resources :about, only: [:index]
+  end
+  resources :users, only: [:index]
 end
