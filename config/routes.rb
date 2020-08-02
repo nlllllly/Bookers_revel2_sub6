8318d18ac56eaabb647d2b794e77d079cf30f46c
vagrant <vagrant@localhost.localdomain>
2020-08-02 03:06:07 +0000

    [update]favorite_function_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
   root 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+
+  end
   resource :home, only: [:index] do
     resources :about, only: [:index]
   end
