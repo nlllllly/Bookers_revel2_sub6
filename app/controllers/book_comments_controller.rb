fe4ae54718373b431499deac35cf92a712a1d3d7
vagrant <vagrant@localhost.localdomain>
2020-08-02 06:36:03 +0000

    [finish]sub3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,14 +1,18 @@
 class BookCommentsController < ApplicationController
+    before_action :authenticate_user!
+
     def create
         book = Book.find(params[:book_id])
         book_comment = current_user.book_comments.new(book_comment_params)
         book_comment.book_id = book.id
         book_comment.save
-        redirect_back(fallback_location: root_path)
+        redirect_to request.referer
     end
     def destroy
-        BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-        redirect_back(fallback_location: root_path)
+        book = Book.find(params[:book_id])
+        book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
+        book_comment.destroy
+        redirect_to request.referer
     end
 
     private
