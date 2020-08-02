31568418def0773e75800696ae41ad975c8fa230
vagrant <vagrant@localhost.localdomain>
2020-08-02 06:37:36 +0000

    [finish]sub3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -10,7 +10,7 @@ class BookCommentsController < ApplicationController
     end
     def destroy
         book = Book.find(params[:book_id])
-        book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
+        book_comment = current_user.book_comments.find_by(id: params[:id], book_id: book.id)
         book_comment.destroy
         redirect_to request.referer
     end
