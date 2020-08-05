761ee3d017d0455c9b0c917928fd498492afd2f8
vagrant <vagrant@localhost.localdomain>
2020-08-05 02:31:15 +0000

    [finish]sub6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,17 +5,17 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
-    if @book_comment.save
-      flash[:success] = "Comment was successfully created."
-    end
-    redirect_to request.referer
+    @book_comment.save
+      # flash[:success] = "Comment was successfully created."
+    # end
+    # redirect_to request.referer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
-    book_comment.destroy
-    redirect_to request.referer
+    @book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
+    @book_comment.destroy
+    # redirect_to request.referer
   end
 
   private
